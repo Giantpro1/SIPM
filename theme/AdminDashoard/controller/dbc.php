16bed2b43cf4a39bac228c5c9ac9d7337b6df4a3
Giantpro1 <shakiruolawale597@gmail.com>
2022-09-24 00:01:30 +0100

    admin db conn

---
+++ theme/AdminDashoard/controller/dbc.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once 'config.php';
+
+class Dbc extends Database{
+    public function regisAdmin($sipmAdminFirstName, $sipmAdminLastName, $sipmAdminEmail, $sipmAdminPassword){
+        $sql = "INSERT INTO simp_admin (sipmAdminFirstName, sipmAdminLastName, sipmAdminEmail, sipmAdminPassword) VALUES (:sipmAdminFirstName, :sipmAdminLastName, :sipmAdminEmail, :sipmAdminPassword)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            'sipmAdminFirstName'=>$sipmAdminFirstName,
+            'sipmAdminLastName'=>$sipmAdminLastName,
+            'sipmAdminEmail'=>$sipmAdminEmail,
+            'sipmAdminPassword'=>$sipmAdminPassword
+        ]);
+        return true;
+    }
+
+    public function FunctionName($sipmAdminEmail){
+        $sql = "SELECT sipmAdminEmail, sipmAdminPassword FROM simp_admin WHERE sipmAdminEmail=:sipmAdminEmail ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            'sipmAdminEmail'=>$sipmAdminEmail
+        ]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function validate_param($value){
+        return (!empty($value));
+   }
+
+   public function user_exitsEmail($sipmAdminEmail){
+    $sql = "SELECT sipmAdminEmail FROM simp_admin WHERE sipmAdminEmail=:sipmAdminEmail";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute([
+     'sipmAdminEmail'=>$sipmAdminEmail
+    ]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+ }
+}
