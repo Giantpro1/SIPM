aed743140e00dc6bedbce580a4cdf2778ef881ab
Giantpro1 <shakiruolawale597@gmail.com>
2022-09-25 00:03:20 +0100

    pendingUser a jax request

--- theme/AdminDashoard/theme/js/pendUser.js
+++ theme/AdminDashoard/theme/js/pendUser.js
@@ -12,4 +12,35 @@ $(document).ready(function(){
             }
         });
     }
+
+    $('body').on('click', '.verifyUser', function(e){
+        e.preventDefault()
+        verifyUser = $(this).attr('id')
+        Swal.fire({
+          title: 'Approve?',
+          text: 'Are you sure you want to approve this user?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: "#FF0000",
+          cancelButtonColor: "blue",
+          confirmButtonText: 'Approve user'
+        }).then((result)=>{
+          if(result.value){
+            $.ajax({
+                url:'../controller/dbAction.php',
+              method:'POST',
+              data: {verifyUser: verifyUser},
+              success: function(response){
+                Swal.fire({
+                icon:'success',
+                text:'You have successfully approved this user',
+                title:'user Approved'
+              })
+              displayPendingUser()
+              }
+            })
+          }
+        })
+       
+      })
 });
