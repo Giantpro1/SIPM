947bb79f3bc38161c405fa27d9f5d57ca017c16c
Giantpro1 <shakiruolawale597@gmail.com>
2022-09-26 21:16:33 +0100

    view verifyUsers

--- theme/AdminDashoard/theme/js/verifyUser.js
+++ theme/AdminDashoard/theme/js/verifyUser.js
@@ -45,4 +45,32 @@ $(document).ready(function(){
         })
        
       })
+
+            // view user
+            $('body').on('click', '.viewUser', function(e){
+              e.preventDefault()
+              viewUser = $(this).attr('id')
+      
+              $.ajax({
+                url: "http://localhost/Giantpro/SIPM/theme/AdminDashoard/controller/api/viewVerifyUser.php",
+                type: "POST",
+                data: {viewUser: viewUser},
+                success: function (response) {
+                  // console.log(response.message)
+                  data = JSON.stringify(response.message)
+                  // console.log(data)
+                  // data = JSON.stringify(response)
+                  // console.log(data)
+                  result = JSON.parse(data)
+                  console.log(result)
+                  $("#viewUserProPic").html("<img class='product-thumb rounded-circle img-fluid' src='../"+result.sipmUser_ProfileImg+"'>")
+                  $("#viewUserName").text(result.simp_UserName)
+                  $("#viewUserEmail").text(result.simpUser_Email)
+                  $("#viewUserUnique").text(result.unique_id)
+                  $("#viewUserFistName").text(result.sipmUser_FirstName)
+                  $("#viewUserSecondName").text(result.sipmUser_SecondName)
+                  $("#viewUserComName").text(result.sipmUser_CommunityName)
+                }
+              });
+            })
 });
